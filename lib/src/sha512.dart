d060821f23e06b288e8e6d117668b63b69958412
Daniel Newton <djpnewton@gmail.com>
2025-06-19 13:37:45 -0400

    javascript int literals dont have enough precision so go directly to bigints

--- lib/src/sha512.dart
+++ lib/src/sha512.dart
@@ -20,34 +20,34 @@ class State {
 final BigInt _maxInt64 = BigInt.parse('FFFFFFFFFFFFFFFF', radix: 16);
 
 // dart format off
-const _k = [
-    0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f,
-    0xe9b5dba58189dbbc, 0x3956c25bf348b538, 0x59f111f1b605d019,
-    0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242,
-    0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
-    0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235,
-    0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3,
-    0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65, 0x2de92c6f592b0275,
-    0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
-    0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f,
-    0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725,
-    0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc,
-    0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
-    0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6,
-    0x92722c851482353b, 0xa2bfe8a14cf10364, 0xa81a664bbc423001,
-    0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218,
-    0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
-    0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99,
-    0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb,
-    0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc,
-    0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
-    0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915,
-    0xc67178f2e372532b, 0xca273eceea26619c, 0xd186b8c721c0c207,
-    0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba,
-    0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
-    0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc,
-    0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a,
-    0x5fcb6fab3ad6faec, 0x6c44198c4a475817,
+final _k = [
+    BigInt.parse('428a2f98d728ae22', radix: 16), BigInt.parse('7137449123ef65cd', radix: 16), BigInt.parse('b5c0fbcfec4d3b2f', radix: 16),
+    BigInt.parse('e9b5dba58189dbbc', radix: 16), BigInt.parse('3956c25bf348b538', radix: 16), BigInt.parse('59f111f1b605d019', radix: 16),
+    BigInt.parse('923f82a4af194f9b', radix: 16), BigInt.parse('ab1c5ed5da6d8118', radix: 16), BigInt.parse('d807aa98a3030242', radix: 16),
+    BigInt.parse('12835b0145706fbe', radix: 16), BigInt.parse('243185be4ee4b28c', radix: 16), BigInt.parse('550c7dc3d5ffb4e2', radix: 16),
+    BigInt.parse('72be5d74f27b896f', radix: 16), BigInt.parse('80deb1fe3b1696b1', radix: 16), BigInt.parse('9bdc06a725c71235', radix: 16),
+    BigInt.parse('c19bf174cf692694', radix: 16), BigInt.parse('e49b69c19ef14ad2', radix: 16), BigInt.parse('efbe4786384f25e3', radix: 16),
+    BigInt.parse('0fc19dc68b8cd5b5', radix: 16), BigInt.parse('240ca1cc77ac9c65', radix: 16), BigInt.parse('2de92c6f592b0275', radix: 16),
+    BigInt.parse('4a7484aa6ea6e483', radix: 16), BigInt.parse('5cb0a9dcbd41fbd4', radix: 16), BigInt.parse('76f988da831153b5', radix: 16),
+    BigInt.parse('983e5152ee66dfab', radix: 16), BigInt.parse('a831c66d2db43210', radix: 16), BigInt.parse('b00327c898fb213f', radix: 16),
+    BigInt.parse('bf597fc7beef0ee4', radix: 16), BigInt.parse('c6e00bf33da88fc2', radix: 16), BigInt.parse('d5a79147930aa725', radix: 16),
+    BigInt.parse('06ca6351e003826f', radix: 16), BigInt.parse('142929670a0e6e70', radix: 16), BigInt.parse('27b70a8546d22ffc', radix: 16),
+    BigInt.parse('2e1b21385c26c926', radix: 16), BigInt.parse('4d2c6dfc5ac42aed', radix: 16), BigInt.parse('53380d139d95b3df', radix: 16),
+    BigInt.parse('650a73548baf63de', radix: 16), BigInt.parse('766a0abb3c77b2a8', radix: 16), BigInt.parse('81c2c92e47edaee6', radix: 16),
+    BigInt.parse('92722c851482353b', radix: 16), BigInt.parse('a2bfe8a14cf10364', radix: 16), BigInt.parse('a81a664bbc423001', radix: 16),
+    BigInt.parse('c24b8b70d0f89791', radix: 16), BigInt.parse('c76c51a30654be30', radix: 16), BigInt.parse('d192e819d6ef5218', radix: 16),
+    BigInt.parse('d69906245565a910', radix: 16), BigInt.parse('f40e35855771202a', radix: 16), BigInt.parse('106aa07032bbd1b8', radix: 16),
+    BigInt.parse('19a4c116b8d2d0c8', radix: 16), BigInt.parse('1e376c085141ab53', radix: 16), BigInt.parse('2748774cdf8eeb99', radix: 16),
+    BigInt.parse('34b0bcb5e19b48a8', radix: 16), BigInt.parse('391c0cb3c5c95a63', radix: 16), BigInt.parse('4ed8aa4ae3418acb', radix: 16),
+    BigInt.parse('5b9cca4f7763e373', radix: 16), BigInt.parse('682e6ff3d6b2b8a3', radix: 16), BigInt.parse('748f82ee5defb2fc', radix: 16),
+    BigInt.parse('78a5636f43172f60', radix: 16), BigInt.parse('84c87814a1f0ab72', radix: 16), BigInt.parse('8cc702081a6439ec', radix: 16),
+    BigInt.parse('90befffa23631e28', radix: 16), BigInt.parse('a4506cebde82bde9', radix: 16), BigInt.parse('bef9a3f7b2c67915', radix: 16),
+    BigInt.parse('c67178f2e372532b', radix: 16), BigInt.parse('ca273eceea26619c', radix: 16), BigInt.parse('d186b8c721c0c207', radix: 16),
+    BigInt.parse('eada7dd6cde0eb1e', radix: 16), BigInt.parse('f57d4f7fee6ed178', radix: 16), BigInt.parse('06f067aa72176fba', radix: 16),
+    BigInt.parse('0a637dc5a2c898a6', radix: 16), BigInt.parse('113f9804bef90dae', radix: 16), BigInt.parse('1b710b35131c471b', radix: 16),
+    BigInt.parse('28db77f523047d84', radix: 16), BigInt.parse('32caab7b40c72493', radix: 16), BigInt.parse('3c9ebe0a15c9bebc', radix: 16),
+    BigInt.parse('431d67c49c100d4c', radix: 16), BigInt.parse('4cc5d4becb3e42b6', radix: 16), BigInt.parse('597f299cfc657e2a', radix: 16),
+    BigInt.parse('5fcb6fab3ad6faec', radix: 16), BigInt.parse('6c44198c4a475817', radix: 16),
 ];
 // dart format on
 
@@ -66,14 +66,14 @@ Uint8List _padData(Uint8List input) {
 
 State _initializeState() {
   return State(
-    BigInt.from(0x6a09e667f3bcc908),
-    BigInt.from(0xbb67ae8584caa73b),
-    BigInt.from(0x3c6ef372fe94f82b),
-    BigInt.from(0xa54ff53a5f1d36f1),
-    BigInt.from(0x510e527fade682d1),
-    BigInt.from(0x9b05688c2b3e6c1f),
-    BigInt.from(0x1f83d9abfb41bd6b),
-    BigInt.from(0x5be0cd19137e2179),
+    BigInt.parse('6a09e667f3bcc908', radix: 16),
+    BigInt.parse('bb67ae8584caa73b', radix: 16),
+    BigInt.parse('3c6ef372fe94f82b', radix: 16),
+    BigInt.parse('a54ff53a5f1d36f1', radix: 16),
+    BigInt.parse('510e527fade682d1', radix: 16),
+    BigInt.parse('9b05688c2b3e6c1f', radix: 16),
+    BigInt.parse('1f83d9abfb41bd6b', radix: 16),
+    BigInt.parse('5be0cd19137e2179', radix: 16),
   );
 }
 
@@ -132,8 +132,7 @@ State _processBlock(Uint8List block, State state) {
 
   // main loop
   for (var i = 0; i < 80; i++) {
-    final term1 =
-        (h + _sum1(e) + _ch(e, f, g) + BigInt.from(_k[i]) + w[i]) & _maxInt64;
+    final term1 = (h + _sum1(e) + _ch(e, f, g) + _k[i] + w[i]) & _maxInt64;
     final term2 = (_sum0(a) + _maj(a, b, c)) & _maxInt64;
 
     h = g;
